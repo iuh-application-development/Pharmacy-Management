0330875fd534a18079d4a301d309a503d08eab2f
truonglongty <137083025+truonglongty@users.noreply.github.com>
2025-03-12 18:16:57 +0700

    Update

--- backend/pharmacy/settings.py
+++ backend/pharmacy/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'medicines.apps.MedicinesConfig',
     'sales.apps.SalesConfig',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,7 +52,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'pharmacy.urls'
 
