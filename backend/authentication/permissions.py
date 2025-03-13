159d4bd13efd9206f51b4bc7254110e0f07160fe
truonglongty <137083025+truonglongty@users.noreply.github.com>
2025-03-13 15:59:58 +0700

    update permission for sales, pm

--- backend/authentication/permissions.py
+++ backend/authentication/permissions.py
@@ -5,3 +5,13 @@ class IsAdminSystem(permissions.BasePermission):
     # Chỉ cho phép admin hệ thống được truy cập
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role.roleName == 'Admin'
+    
+class IsSales(permissions.BasePermission):
+    # Chỉ cho phép nhân viên bán hàng hoặc admin được truy cập
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role.roleName in ['Sales', 'Admin']
+    
+class IsProductManager(permissions.BasePermission):
+    # Chỉ cho phép quản lý sản phẩm hoặc admin được truy cập
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role.roleName in ['Product_manager', 'Admin']
