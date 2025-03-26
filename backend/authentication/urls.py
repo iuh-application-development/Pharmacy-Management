bc87affc3290b0dcad7a5adf6b76196de1f1269f
Hellouyne2004 <thaiuy1006@gmail.com>
2025-03-26 18:41:20 +0700

    Remove unnecessary parts in views

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter, SimpleRouter
-from .views import EmployeeViewSet, AccountViewSet, RoleViewSet, StatisticViewSet, EmployeeStatisticViewSet, LoginView, LogoutView, MeView, ResetPasswordView
+from .views import EmployeeViewSet, AccountViewSet, RoleViewSet, LoginView, LogoutView, MeView, ResetPasswordView
 
 # Router cho CRUD (có sẵn các phương thức GET, POST, PUT, DELETE)
 router = DefaultRouter()
@@ -8,15 +8,10 @@ router.register('employees', EmployeeViewSet, basename='employee')
 router.register('accounts', AccountViewSet, basename='account')
 router.register('roles', RoleViewSet, basename='role')
 
-# Router cho các phương thức thống kê (chỉ có phương thức GET)
-statistic_router = SimpleRouter()
-statistic_router.register('statistics/finance', StatisticViewSet, basename='finance-statistic')
-statistic_router.register('statistics/employee', EmployeeStatisticViewSet, basename='employee-statistic')
 
 # urls: /api/auth/
 urlpatterns = [
     path('', include(router.urls)),
-    path('', include(statistic_router.urls)),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('me/', MeView.as_view(), name='me'),
