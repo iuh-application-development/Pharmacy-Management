c3e37a2a23f6fe07866a258d0dcaf0327e7720c2
truonglongty <137083025+truonglongty@users.noreply.github.com>
2025-04-27 16:29:59 +0700

    fix(authentication): ensure password is hashed when updating accounts

--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -40,3 +40,12 @@ class AccountSerializer(serializers.ModelSerializer):
         account.save()
         return account
 
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password) 
+        instance.save()
+        return instance
+
