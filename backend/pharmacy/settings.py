562ac23b4e422001bd05204158148567e0212a3c
truonglongty <137083025+truonglongty@users.noreply.github.com>
2025-05-26 16:08:47 +0700

    feat: add Docker Compose setup for backend, frontend, and PostgreSQL deployment

--- backend/pharmacy/settings.py
+++ backend/pharmacy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,14 +92,13 @@ WSGI_APPLICATION = 'pharmacy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pharmacydb',  
-        'USER': 'postgres',  
-        'PASSWORD': '123456', 
-        'HOST': 'localhost',
-        'PORT': '5432',  
+        'NAME': os.environ.get('POSTGRES_DB', 'pharmacydb'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', '123456'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
